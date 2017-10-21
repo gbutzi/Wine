57912addd80138d22956ac64fb32c843f2dc8991
Greg Butzi <greg@butzi.net>
2017-10-20 17:05:53 -0700

    starting to change Bottle.py to subclass dict

--- Bottle.py
+++ Bottle.py
@@ -1,51 +1,51 @@
 import hashlib
 import pdb
 
-class Bottle:
+class Bottle(dict):
 
     #{"quantity": int, "index": int, "name": "a string", "review" = ["a list", "of strings"]}
 
     def __init__(self, tagstring):
-        self.index = 0
-        self.tags = []
-        self.quantity = 0
-        self.review = []
+        self['index'] = 0
+        self['tags'] = []
+        self['quantity'] = 0
+        self['review'] = []
         lowtags = tagstring.lower()
         listing = lowtags.split()
         for i in listing:
-            self.tags.append(i)
-        self.tags.sort()
-        self.name = tagstring
-        self.quantity += 1
-        self.index = 0
-        self.price = 0
+            self['tags'].append(i)
+        self['tags'].sort()
+        self['name'] = tagstring
+        self['quantity'] += 1
+        self['index']= 0
+        self['price'] = 0
 
     def drink(self):
-        self.quantity -= 1
+        self['quantity'] -= 1
         print("What do you think?")
         thoughts = input()
         print("What is the date?")
         date = input()
         r = date + ": " + thoughts
-        self.review.append(r)
+        self['review'].append(r)
 
     def setIndex(self, i):
-        self.index = i
+        self['index'] = i
 
     def printTags(self):
-        for i in self.tags:
+        for i in self['tags']:
             print(i)
 
     def view(self):
-        print(self.name)
-        print("In Stock: " + str(self.quantity))
-        for i in self.review:
+        print(self['name'])
+        print("In Stock: " + str(self['quantity']))
+        for i in self['review']:
             print(i)
 
     def __eq__(self, bottle):
         if not isinstance(bottle, Bottle): return False
-        selftags = ' '.join(sorted(self.tags))
-        cmptags = ' '.join(sorted(bottle.tags))
+        selftags = ' '.join(sorted(self['tags']))
+        cmptags = ' '.join(sorted(bottle['tags']))
 
         sHasher = hashlib.sha256()
         cHasher = hashlib.sha256()
@@ -71,15 +71,15 @@ class Bottle:
             return None
         elif choice == 'quantity' or choice == '1':
             print("Change quantity to what?")
-            self.quantity = int(input())
-            print("Quantity is now " + str(self.quantity))
+            self['quantity'] = int(input())
+            print("Quantity is now " + str(self['quantity']))
         elif choice == 'review' or choice == '2':
             print("What do you think?")
             thoughts = input()
             print("What is the date?")
             date = input()
             r = date + ": " + thoughts
-            self.review.append(r)
+            self['review'].append(r)
         else:
             return None
 
