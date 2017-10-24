6281210a08e54d5d029b28ef005eaaf89fb95e3a
Greg Butzi <greg@butzi.net>
2017-10-24 16:36:55 -0700

    now i have an update script that copies stuff over from refactor

---
+++ update.fish
@@ -0,0 +1,5 @@
+#!/usr/local/bin/fish
+
+command cp /home/greg/Wine/refactor/Wine.py /home/greg/Wine/Wine.py
+command cp /home/greg/Wine/refactor/Rack.py /home/greg/Wine/Rack.py
+command cp /home/greg/Wine/refactor/Bottle.py /home/greg/Wine/Bottle.py
